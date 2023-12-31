255843f63fce846961b14010114b1f0c99ab020a
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-01-01 00:21:18 +0530

    added: 01-Functions & type specific param

---
+++ 01-Functions/defineFunctions.ts
@@ -0,0 +1,10 @@
+function add (a: number, b: number){
+    return a + b;
+}
+
+const result = add(1, 2);
+
+console.log(result)
+
+// to compile and generate js code use tsc fileName.ts
+// it will generate a fileName.js which node can run easily
