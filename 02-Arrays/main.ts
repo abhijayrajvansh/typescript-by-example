f6c74f8dc1f7f4aaccaa9ce24f147f133f8ca111
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-01-06 08:56:33 +0530

    update: Array<T>

--- 02-Arrays/main.ts
+++ 02-Arrays/main.ts
@@ -7,7 +7,7 @@ let anotherArr: Array <number | string> = []
 let session: Array<string> = ['lohit', 'abhijay', 'keshav', 'mehul', 'karmik', 'indram'] // this is preferred
 
 const tuple: [number, number, number] = [2711, 9321, 345] // ✅
-const anotherTuple: [number, number, number] = [2711, 9321, 345, 999] // ❌
+// const anotherTuple: [number, number, number] = [2711, 9321, 345, 999] // ❌
 // source has 4 elements but targeted only three
 
 console.log(session)
