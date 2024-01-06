dab98adf41f7f9becc99a2896b2c4ba83b7f56be
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-01-06 08:53:35 +0530

    update: Array<T>

--- 02-Arrays/main.ts
+++ 02-Arrays/main.ts
@@ -4,6 +4,10 @@ let anotherArr: Array <number | string> = []
 
 // interface Array<T> where T is Type!
 
-let session: Array<string> = ['lohit', 'abhijay', 'keshav', 'mehul', 'indram'] // this is preferred
+let session: Array<string> = ['lohit', 'abhijay', 'keshav', 'mehul', 'karmik', 'indram'] // this is preferred
+
+const tuple: [number, number, number] = [2711, 9321, 345] // ✅
+const anotherTuple: [number, number, number] = [2711, 9321, 345, 999] // ❌
+// source has 4 elements but targeted only three
 
 console.log(session)
