b47aef74cc2cb16dbf97c4b745eb40b98f153869
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-01-06 08:48:51 +0530

    update: Array<T>

---
+++ 02-Arrays/main.ts
@@ -0,0 +1,9 @@
+let arr: number[] = [1, 2, 3, 4, 5] // this is good and IDE supported
+let anotherArr: Array <number | string> = []
+// here the anotherArr is statically typed
+
+// interface Array<T> where T is Type!
+
+let session: Array<string> = ['lohit', 'abhijay', 'keshav', 'mehul', 'indram'] // this is preferred
+
+console.log(session)
